7fca9719e1f48ee3cfd8ead537396f2fb909e3dd
Juan Huelmo <juanhuelmo@192.168.1.18>
2025-06-08 23:22:10 -0300

    Initial commit of Angular project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",  // Esto indica dónde buscar clases tailwind (templates y TS)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
